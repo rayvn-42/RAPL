ff2bd228569151060284004d5effff53e39268a8
RAYANEPROGRAMMER <108599429+RAYANEPROGRAMMER@users.noreply.github.com>
2024-09-29 20:16:54 +0100

    Update Shell.py

--- Shell.py
+++ Shell.py
@@ -1,14 +1,21 @@
 import rapl
 
 while True:
-	text = input('rapl> ')
-	if text.strip() == "": continue
-	result, error = rapl.run('<stdin>', text)
+    try:
+        text = input('rapl> ')
+        if text.strip() == "":
+            continue
+        result, error = rapl.run('<stdin>', text)
 
-	if error:
-		print(error.as_string())
-	elif result:
-		if len(result.elements) == 1:
-			print(repr(result.elements[0]))
-		else:
-			print(repr(result))
+        if error:
+            print(error.as_string())
+        elif result:
+            if len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
+    except KeyboardInterrupt:
+        print("\nExiting RAPL.")
+        break
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
